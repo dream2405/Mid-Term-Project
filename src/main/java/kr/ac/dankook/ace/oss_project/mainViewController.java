0d0892d28ea686269d04c93068c908f383eda1c9
dream2405 <dream2405@naver.com>
2024-04-18 22:54:12 +0900

    탭 완성

--- src/main/java/kr/ac/dankook/ace/oss_project/mainViewController.java
+++ src/main/java/kr/ac/dankook/ace/oss_project/mainViewController.java
@@ -3,11 +3,21 @@ package kr.ac.dankook.ace.oss_project;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class mainViewController {
     @GetMapping("/")
     public String mainView(Model model) {
+        model.addAttribute("tabMenu", "intro");
+
+        return "main";
+    }
+
+    @GetMapping("/{tabMenu}")
+    public String introView(@PathVariable String tabMenu, Model model) {
+        System.out.println(tabMenu);
+        model.addAttribute("tabMenu", tabMenu);
 
         return "main";
     }
