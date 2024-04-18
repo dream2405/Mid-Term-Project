1c6809d41ba7d20a9f0526c32f3758364e7a5501
dream2405 <dream2405@naver.com>
2024-04-18 20:19:27 +0900

    Initial commit

---
+++ src/test/java/kr/ac/dankook/ace/oss_project/OssProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.dankook.ace.oss_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
